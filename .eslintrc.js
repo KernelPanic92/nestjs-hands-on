626d090c10b3a67dac492f144f8602d0846a2c1b
iacopo ciao <iacopociao1992@gmail.com>
2020-11-20 15:40:47 +0100

    feat: make type check restrictive

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,9 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+    '@typescript-eslint/explicit-module-boundary-types': ['error'],
+    '@typescript-eslint/no-explicit-any': ['warning'],
+    '@typescript-eslint/explicit-member-accessibility': ['error'],
   },
 };
