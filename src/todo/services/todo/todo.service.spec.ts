7bbe000ffc3148f2abaea95289ee69e5adf35a14
iacopo ciao <iacopociao1992@gmail.com>
2020-12-11 19:22:18 +0100

    test(todo.service.spec): add missing tests and correcting existing tests

--- src/todo/services/todo/todo.service.spec.ts
+++ src/todo/services/todo/todo.service.spec.ts
@@ -1,3 +1,4 @@
+import { TodoDto } from '../../dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Todo } from '../../entities/todo.entity';
@@ -9,6 +10,7 @@ import { Repository } from 'typeorm';
 import { TodoMapperService } from '../todo-mapper/todo-mapper.service';
 import { TodoService } from './todo.service';
 import { CreateTodoDto, UpdateTodoDto } from '../../dto';
+import { NotFoundException } from '@nestjs/common';
 
 describe('TodoService', () => {
   let service: TodoService;
@@ -60,23 +62,33 @@ describe('TodoService', () => {
     expect(repository.findOne).toBeCalledTimes(1);
   });
 
+  it('should return TodoDto in find one', async () => {
+    repository.findOne.mockReturnValue(Promise.resolve(new Todo('title')));
+
+    const actual = await service.findOne(1);
+
+    expect(actual.constructor).toBe(TodoDto);
+  });
+
   it('should throws error when todo not exists', async () => {
     repository.findOne.mockReturnValue(Promise.resolve(null));
+    repository.count.mockReturnValue(Promise.resolve(0));
+    repository.findOneOrFail.mockRejectedValue(new Error('fail') as never);
 
-    await expect(service.findOne(1)).rejects.toThrow();
+    await expect(service.findOne(1)).rejects.toThrow(NotFoundException);
   });
 
   it('should update todo', async () => {
     const todo = new Todo('title');
     repository.findOne.mockReturnValue(Promise.resolve(todo));
-    repository.save.mockReturnValue(todo);
+    repository.save.mockReturnValue(Promise.resolve(todo));
 
     await service.update(1, new UpdateTodoDto());
 
     expect(repository.save).toBeCalledTimes(1);
   });
 
-  it('should delete todo', async () => {
+  it('should remove todo', async () => {
     const todo = new Todo('title');
     repository.findOne.mockReturnValue(Promise.resolve(todo));
     repository.remove.mockReturnValue(Promise.resolve(todo));
@@ -86,9 +98,20 @@ describe('TodoService', () => {
     expect(repository.remove).toBeCalledTimes(1);
   });
 
+  it('should return TodoDto in remove', async () => {
+    repository.findOne.mockReturnValue(Promise.resolve(new Todo('title')));
+    repository.remove.mockReturnValue(Promise.resolve(new Todo('title')));
+
+    const actual = await service.remove(1);
+
+    expect(actual.constructor).toBe(TodoDto);
+  });
+
   it('should throws exception when todo not exists on remove', async () => {
     repository.findOne.mockReturnValue(Promise.resolve(null));
+    repository.count.mockReturnValue(Promise.resolve(0));
+    repository.findOneOrFail.mockRejectedValue(new Error('fail') as never);
 
-    await expect(service.remove(1)).rejects.toThrow();
+    await expect(service.remove(1)).rejects.toThrow(NotFoundException);
   });
 });
