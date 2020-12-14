b6deb11901901fb1d67a5c71c009db76af93108e
iacopo ciao <iacopociao1992@gmail.com>
2020-12-14 09:20:05 +0100

    feat: use type check in rest api
    (cherry picked from commit fa3f42307ee41a2833d8f39c61088c88e2e9cbc4)

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,4 +1,8 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
   public title: string;
 
   public constructor(args?: Partial<CreateTodoDto>) {
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,5 +1,11 @@
+import { IsString, IsNotEmpty, IsBoolean } from 'class-validator';
+
 export class UpdateTodoDto {
+  @IsString()
+  @IsNotEmpty()
   public title: string;
+
+  @IsBoolean()
   public completed: boolean;
 
   public constructor(opts?: Partial<UpdateTodoDto>) {
